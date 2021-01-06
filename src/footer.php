f0fc0d2b91c0868f1c78040a47fced2c3d5690c6
Bence Agocs <bence@harom.studio>
2021-01-06 13:07:29 +0100

    home clients time shortened

--- src/footer.php
+++ src/footer.php
@@ -152,36 +152,36 @@
 			<script type="text/javascript">
 				$(document).ready(function(){
 				
-				var logos = $('.clients .logos-container');
-				var fadeTime = $('.clients').attr('data-fade-time');  // You can set fadeing-transition time;
-				
-				logos.each(function() {
-					var next = 1;      // fixed, please do not modfy;
-					var current = 0;   // fixed, please do not modfy;
-					var imgs = $(this).find('img');
-					var imgNum = imgs.length;    // How many pictures do you have
-					var interval = 4000; // You can set single picture show time;
-					var delay = (parseInt($(this).find('.data-interval').text()))*1000;
-				
-					setTimeout(function(){
-						nextFadeIn();
-						console.table(imgs);
-						console.log('interval: ' + interval);
-						console.log('delay: ' + delay);
-					}, delay);
-				
-					function nextFadeIn(){
-					//make image fade in and fade out at one time, without splash vsual;
-					imgs.eq(current).delay(interval).fadeOut(fadeTime)
-					.end().eq(next).delay(interval).hide().fadeIn(fadeTime, nextFadeIn);
-						
-					// if You have 5 images, then (eq) range is 0~4 
-					// so we should reset to 0 when value > 4; 
-					if(next < imgNum-1){ next++; } else { next = 0;}
-					if(current < imgNum-1){ current++; } else { current = 0; }
-					};
-				
-				});
+					var logos = $('.clients .logos-container');
+					var fadeTime = $('.clients').attr('data-fade-time');  // You can set fadeing-transition time;
+					
+					logos.each( setInterval(function() {
+						var next = 1;      // fixed, please do not modfy;
+						var current = 0;   // fixed, please do not modfy;
+						var imgs = $(this).find('img');
+						var imgNum = imgs.length;    // How many pictures do you have
+						var interval = 4000; // You can set single picture show time;
+						var delay = (parseInt($(this).find('.data-interval').text()))*1000;
+					
+						setTimeout(function(){
+							nextFadeIn();
+							console.table(imgs);
+							console.log('interval: ' + interval);
+							console.log('delay: ' + delay);
+						}, delay);
+					
+						function nextFadeIn(){
+						//make image fade in and fade out at one time, without splash vsual;
+						imgs.eq(current).delay(interval).fadeOut(fadeTime)
+						.end().eq(next).delay(interval).hide().fadeIn(fadeTime, nextFadeIn);
+							
+						// if You have 5 images, then (eq) range is 0~4 
+						// so we should reset to 0 when value > 4; 
+						if(next < imgNum-1){ next++; } else { next = 0;}
+						if(current < imgNum-1){ current++; } else { current = 0; }
+						};
+					
+					}, 10000));
 				
 				});
 			</script>
